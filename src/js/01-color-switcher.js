d62b8305ee2f334dac8575641ad83803b73048e5
YuriyMikh <pamukkale1717@gmail.com>
2023-04-19 21:56:39 +0300

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButtonRef = document.querySelector('button[data-start]');
+const stopButtonRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
 
+stopButtonRef.disabled = true;
+
+startButtonRef.addEventListener('click', event => {
+    startButtonRef.disabled = true;
+    stopButtonRef.disabled = false;
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButtonRef.addEventListener('click', event => {
+    startButtonRef.disabled = false;
+    stopButtonRef.disabled = true;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
