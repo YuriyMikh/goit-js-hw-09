94740f95d8fe15bf05b652f1040943558429bae9
YuriyMikh <pamukkale1717@gmail.com>
2023-04-20 23:57:42 +0300

    added obj-settings

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,19 +4,19 @@ import 'flatpickr/dist/flatpickr.min.css';
 const startButtonRef = document.querySelector('button[data-start]');
 const inputRef = document.querySelector('#datetime-picker');
 
-// const options = {
-//   enableTime: true,
-//   time_24hr: true,
-//   defaultDate: new Date(),
-//   minuteIncrement: 1,
-//   onClose(selectedDates) {
-//     console.log(selectedDates[0]);
-//   },
-// };
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
 startButtonRef.disabled = true;
 
 flatpickr(inputRef, {});
 // if (3 > 2) {
-//   alert('yess');
+//   alert('"Please choose a date in the future"');
 // }
