087f7a2721c5dc317fac49a04b9c60b11099a42c
YuriyMikh <pamukkale1717@gmail.com>
2023-04-20 23:47:02 +0300

    added div-container

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,22 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const startButtonRef = document.querySelector('button[data-start]');
+const inputRef = document.querySelector('#datetime-picker');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+startButtonRef.disabled = true;
+
+flatpickr(inputRef, { options });
+// if (3 > 2) {
+//   alert('yess');
+// }
