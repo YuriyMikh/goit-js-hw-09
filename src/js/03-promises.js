8488a7b2c9f8faf0d01b8b1837a287f6e87a95c1
YuriyMikh <pamukkale1717@gmail.com>
2023-04-25 23:45:17 +0300

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,23 @@
 import Notiflix from 'notiflix';
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+console.log(5);
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', createPromise);
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//     position('ja');
+//   } else {
+//     // Reject
+//     delay('nein');
+//   }
+// }
+
+createPromise()
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+
+  
