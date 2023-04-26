69b9a25870ec91bd269e7c47b791242ce5629503
YuriyMikh <pamukkale1717@gmail.com>
2023-04-26 23:42:03 +0300

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,23 +1,50 @@
 import Notiflix from 'notiflix';
 
-console.log(5);
-
 const formRef = document.querySelector('.form');
 formRef.addEventListener('submit', createPromise);
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//     position('ja');
-//   } else {
-//     // Reject
-//     delay('nein');
-//   }
-// }
+// let delayValue = formRef.elements.delay.value;
+// console.log(delayValue);
+
+function handleSubmitValue(event) {
+  event.preventDefault();
+  return console.log(formRef.elements.delay.value);
+}
+
+function createPromise(position, delay) {
+  const DELAY = formRef.elements.delay.value;
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        // Fulfill
+        resolve({
+          position: 5,
+          delay: formRef.elements.delay.value,
+        });
+      } else {
+        // Reject
+        reject({
+          position: formRef.elements.amount.value,
+          delay: formRef.elements.delay.value,
+        });
+      }
+    }, DELAY);
+  });
+}
 
 createPromise()
-  .then(result => console.log(result))
-  .catch(error => console.log(error));
+  .then(result => {
+    console.log(formRef.elements.delay.value);
+  })
+  .catch(error => error);
 
-  
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
