1f48b57709b3261d6130ed7e21eba761b58b8fcd
YuriyMikh <pamukkale1717@gmail.com>
2023-04-27 16:59:12 +0300

    updeted task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -13,23 +13,27 @@ let intervalId = null;
 let selectedDate = null;
 let currentDate = null;
 
-startButtonRef.disabled = true;
+startButtonRef.disabled = true; //делаем неактивной кнопку Start
 
 startButtonRef.addEventListener('click', onStartCounter);
 
+//переменная с настройками для будущего вызова flatpickr()
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    //если выбранное время в прошлом (меньше чем Date.now()), то выводим ошибку
     if (selectedDates[0].getTime() < Date.now()) {
       Notiflix.Report.failure(
         'Error',
         'Please choose a date in the future',
         'OK'
       );
-    } else {
+    }
+    //а если время в будущем = выводим успех
+    else {
       selectedDate = selectedDates[0].getTime();
       startButtonRef.disabled = false;
       Notiflix.Report.success('Great', 'Now press the "Start" button', 'ОК');
@@ -43,6 +47,7 @@ function onStartCounter() {
   counter.start();
 }
 
+//функция для конвертации милисекунд в дни/часы/минуты/секунды
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -60,7 +65,6 @@ function convertMs(ms) {
   const seconds = addLeadingZero(
     Math.floor((((ms % day) % hour) % minute) / second)
   );
-
   return { days, hours, minutes, seconds };
 }
 
@@ -68,11 +72,13 @@ const counter = {
   start() {
     intervalId = setInterval(() => {
       currentDate = Date.now();
+      console.log(selectedDate);
       const deltaTime = selectedDate - currentDate;
       updateTimerFace(convertMs(deltaTime));
       startButtonRef.disabled = true;
       inputRef.disabled = true;
 
+      //обрабатываем окончание работы секундомера:
       if (deltaTime <= 1000) {
         clearInterval(intervalId);
         startButtonRef.disabled = true;
@@ -82,6 +88,7 @@ const counter = {
   },
 };
 
+//функция для обновления интерфейса
 function updateTimerFace({ days, hours, minutes, seconds }) {
   daysRef.textContent = `${days}`;
   hoursRef.textContent = `${hours}`;
@@ -89,6 +96,7 @@ function updateTimerFace({ days, hours, minutes, seconds }) {
   secondsRef.textContent = `${seconds}`;
 }
 
+//функция добавления нуля перед значением (для корректного отображения часов/минут/секунд)
 function addLeadingZero(value) {
   return value.toString().padStart(2, 0);
 }
